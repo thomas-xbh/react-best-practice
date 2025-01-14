789c99037f2d6d6066a1f0e17023713872962b3c
thomas <thomas.xbh@gmail.com>
2025-01-14 17:07:14 +0800

    feat:引入antd

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { Button } from 'antd';
 import { observer } from "mobx-react-lite";
-import store from "@/store/index";
+import store from "./store/index";
 
 const Home: React.FC = () => {
   return (
     <>
       <h1>欢迎来到首页！</h1>;<h2>计数器：{store.count}</h2>
-      <button onClick={() => store.increment()}>增加</button>
-      <button onClick={() => store.decrement()}>减少</button>
+      <Button type="primary" onClick={() => store.increment()}>增加</Button>
+      <Button type="default" onClick={() => store.decrement()}>减少</Button>
     </>
   );
 };
